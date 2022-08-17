ebbbbdf0022c598ddea2c3210dbbcc02ec451afb
Parker Parham <parkerparham@gmail.com>
2022-08-17 15:33:38 -0400

    Addint ability to fetch based on selected liquor

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     //functions go here
-    displayDrinks()
+    // displayDrinks()
+    enableLiquorForm()
 })
 
 function displayDrinks(){
     fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=red_wine')
     .then((response) => response.json())
     .then((drinkData) => console.log(drinkData))
+}
+
+let liquorForm = document.getElementById('liquor-selector')
+let liquorSelect = document.getElementById('liquor')
+// console.log(liquorSelect)
+
+function enableLiquorForm(){
+    liquorForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let liquorValue = liquorSelect.options[liquorSelect.selectedIndex].value
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${liquorValue}`)
+        .then((response) => response.json())
+        .then((drinkData) => console.log(drinkData))
+    })
 }
